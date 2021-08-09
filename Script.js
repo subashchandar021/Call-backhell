5da1b801d704ab85b29942b9912f9bbd93f79ab4
subashchandar021 <82366513+subashchandar021@users.noreply.github.com>
2021-08-09 20:03:05 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,34 @@
+let countdown=10;
+setTimeout(function(){
+    document.getElementById("mms").innerHTML=countdown--;
+    setTimeout(function(){
+        document.getElementById("mms").innerHTML=countdown--;
+        setTimeout(function(){
+            document.getElementById("mms").innerHTML=countdown--;
+            setTimeout(function(){
+                document.getElementById("mms").innerHTML=countdown--;
+                setTimeout(function(){
+                    document.getElementById("mms").innerHTML=countdown--;
+                    setTimeout(function(){
+                        document.getElementById("mms").innerHTML=countdown--;
+                        setTimeout(function(){
+                            document.getElementById("mms").innerHTML=countdown--;
+                            setTimeout(function(){
+                                document.getElementById("mms").innerHTML=countdown--;
+                                setTimeout(function(){
+                                    document.getElementById("mms").innerHTML=countdown--;
+                                    setTimeout(function(){
+                                        document.getElementById("mms").innerHTML=countdown--;
+                                        setTimeout(function(){
+                                            document.getElementById("mms").innerHTML="HAPPY INDEPENDENCE DAY 2021";
+                                            },1000)
+                                        },1000)
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
